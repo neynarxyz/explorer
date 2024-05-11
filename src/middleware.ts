9e04d1620d3e063d39f17cd56c5cb443852ddfdc
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-10 18:16:09 -0700

    try root of src

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,9 +10,6 @@ export function middleware(req: NextRequest) {
     const scheme = process.env.VERCEL_ENV !== 'development' ? 'https://' : 'http://';
     const website = process.env.VERCEL_URL || 'localhost:3000';
     const allowedOrigin = scheme + website;
-    console.log('allowedOrigin', allowedOrigin)
-    console.log('pathname', pathname)
-    console.log("website", website)
 
     if (pathname.startsWith(`/api/`)) {
         if (!req.headers.get("referer")?.includes(website)) {
