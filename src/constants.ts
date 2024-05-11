d49627806e64270e31fb04aab4d67532b0846266
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-10 17:28:54 -0700

    first go at this

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const hubs = [
+    {"shortname": "neynar", "url": "https://hub-api.neynar.com"},
+    {"shortname": "nemes", "url": "https://nemes.farcaster.xyz:2281"},
+    {"shortname": "hoyt", "url": "https://hoyt.farcaster.xyz:2281"},
+    {"shortname": "lamia", "url": "https://lamia.farcaster.xyz:2281"},
+    {"shortname": "standard_crypto", "url": "https://hub.farcaster.standardcrypto.vc:2281"},
+    {"shortname": "wevm", "url": "https://api.hub.wevm.dev"},
+    {"shortname": "pinata", "url": "https://hub.pinata.cloud"},
+    {"shortname": "farcasthub", "url": "http://api.farcasthub.com:2281"},
+  ]
+  export const neynarHub = {"shortname": "neynar", "url": "https://hub-api.neynar.com"}
+  export const defaultHash = "0x3e7326f8da760ed926c1fe82e1444cd528fe6c78"
+  
