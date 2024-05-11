d29e9818f8eda652898151a2dbd7a6f12430b970
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-10 18:27:07 -0700

    check if origin is equal to site

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,12 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(req: NextRequest) {
-
     const url = req.nextUrl
-    const { pathname } = url
     const scheme = process.env.VERCEL_ENV !== 'development' ? 'https://' : 'http://';
     const website = process.env.VERCEL_URL || 'localhost:3000';
     const allowedOrigin = scheme + website;
-    console.log('allowedOrigin', allowedOrigin)
-    console.log("website", website)
-    console.log("pathname", pathname)
-    console.log("req",req)
-
-    if (pathname.startsWith(`/api/`)) {
-        if (!req.headers.get("referer")?.includes(website)) {
+        if (url.origin !== allowedOrigin) {
         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-        }
       }
 
      return NextResponse.next()
