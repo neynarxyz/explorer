199ed6dd1c64ff79a88d4267c34d7ed3d9980c45
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-10 17:37:13 -0700

    fix placeholders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,14 +29,14 @@ fetchData(defaultHash, fid);
     <div className="flex flex-col items-center justify-center space-y-4 p-2">
       <Input
         className="w-64 h-10"
-        placeholder={defaultHash}
+        placeholder={`Enter hash... (${defaultHash.slice(0,5)}...)`}
         value={hash}
         onChange={(e: any) => {
           setHash(e.target.value)}}
       />
       <Input
         className="w-64 h-10"
-        placeholder="FID (optional unless API fails)"
+        placeholder="Enter FID (optional unless API fails)"
         type="number"
         value={fid ?? ''}
         onChange={(e: any) => setFid(Number(e.target.value) || null)}
@@ -45,8 +45,6 @@ fetchData(defaultHash, fid);
       <div className="flex flex-col items-center justify-center space-y-4 p-2 w-full">
      {castPreview ? <><p>Result for {castPreview.hash}</p><FarcasterEmbed username={castPreview.author.username} hash={castPreview.hash.slice(0, 10)} /></> : null}
         </div>
-
-
       <div>
         <div>Neynar API: {displayResponseStatus(apiResponse)}</div>
         <div>
