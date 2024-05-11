176a1486dfd945163b2ff01eac64b51d4ebdc53d
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-10 18:08:04 -0700

    switch up middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,31 @@
+
 import { NextResponse } from 'next/server'
+
 import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  const scheme = process.env.VERCEL_ENV !== 'development' ? 'https://' : 'http://';
-  const website = process.env.VERCEL_URL || 'localhost:3000';
-  const allowedOrigin = scheme + website;
-  const requestOrigin = request.headers.get('origin');
-  if (requestOrigin && requestOrigin === allowedOrigin) {
-    return NextResponse.next()
-  }
-  return Response.json({
-    status: 403,
-    body: 'Unauthorized',
-  })
+
+export function middleware(req: NextRequest) {
+
+    const url = req.nextUrl
+    const { pathname } = url
+    const scheme = process.env.VERCEL_ENV !== 'development' ? 'https://' : 'http://';
+    const website = process.env.VERCEL_URL || 'localhost:3000';
+    const allowedOrigin = scheme + website;
+    console.log('allowedOrigin', allowedOrigin)
+    console.log('pathname', pathname)
+    console.log("website", website)
+
+    if (pathname.startsWith(`/api/`)) {
+        if (!req.headers.get("referer")?.includes(website)) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+        }
+      }
+
+     return NextResponse.next()
+
 }
- 
+
 export const config = {
-  matcher: '/api/:path*',
+  matcher: ['/((?!_next|fonts|examples|svg|[\\w-]+\\.\\w+).*)'],
 }
 
   
