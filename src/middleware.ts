7918fe62203c43323a0f906ee16c3aea6ad4e051
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-23 14:29:02 -0700

    add middle ware back

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+
+import { NextResponse } from 'next/server'
+
+import type { NextRequest } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const env = process.env.VERCEL_ENV ?? 'development'
+  if(env === 'development') {
+    return NextResponse.next()
+  }
+
+    //check if url has Authentication header passed in
+    if (!req.headers.get('Authorization')) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+      }
+      //get authorization header
+      const authHeader = req.headers.get('Authorization');
+      //Add better token flow
+  if(authHeader !== process.env.NEXT_PUBLIC_TOKEN_SECRET) {
+        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+      }
+
+     return NextResponse.next()
+
+}
+
+export const config = {
+  matcher: '/api/:path*',
+}
