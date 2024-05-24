af85d99dbe8e8f89a8d13e79258d92e989c32193
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-23 23:25:43 -0700

    always have button and input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,60 @@
 
+'use client'
 import "./globals.css";
 import "react-farcaster-embed/dist/styles.css";
 import Link from "next/link";
 import Providers from "./providers";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next13-progressbar";
+import { useState } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [identifier, setIdentifier] = useState("");
+  const router = useRouter();
+
+  const handleKeyPress = (e: any) => {
+    if (e.key === "Enter") {
+      router.push(`/${encodeURIComponent(identifier)}`);
+    }
+  };
+
   return ( 
     <html lang="en">
       <Providers>
-      <body className="relative flex flex-col flex-1 justify-center items-center h-full">
-        <div className="absolute left-2 top-4 px-10 w-full flex items-center justify-between">
-          <Link href="/" className="text-black font-bold">
-            <p className="text-lg md:text-xl">Farcaster Explorer</p>
-          </Link>
-          <Link href="https://www.neynar.com" target="_blank">
-            <img className="w-16 md:w-32" src={"/neynar.png"} />
-          </Link>
-        </div>
-
-        <div className="mt-16 md:mt-20 w-full px-4 md:px-8">
-          {children}
-        </div>
-      </body>
+        <body className="flex flex-col flex-1 items-center h-full">
+          <div className="w-full flex items-center justify-between px-10 py-2">
+            <Link href="/" className="text-black font-bold">
+              <p className="text-lg md:text-xl">Farcaster Explorer</p>
+            </Link>
+       
+            <Link href="https://www.neynar.com" target="_blank">
+              <img className="w-16 md:w-32" src={"/neynar.png"} />
+            </Link>
+          </div>
+          <div className="flex flex-col items-center w-full space-y-4">
+                <Input
+                  className="w-full h-10 md:w-96"
+                  placeholder="Enter a FID, hash, or warpcast url..."
+                  value={identifier}
+                  onChange={(e) => {
+                    setIdentifier(e.target.value)}}
+                  onKeyDown={handleKeyPress}
+                />
+                <Button className="" onClick={() => router.push(`/${encodeURIComponent(identifier)}`)}>
+                  Fetch Identifier
+                </Button>
+              </div>
+          <div className=" w-full px-4 md:px-8">
+            {children}
+          </div>
+        </body>
       </Providers>
     </html>
+);
 
-  );
 }
