791e2adcf8f6310c8b0988b157b98f058c7d320a
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-23 17:34:53 -0700

    switch to progress bar for nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next13-progressbar";
 import { CastComponent } from '@/components/cast-component';
 
 export default function Home() {
