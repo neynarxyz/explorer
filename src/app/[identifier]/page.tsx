894f51e48efcdab7f8b2a8c845e843a12017dab3
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-05-29 15:23:29 -0700

    make collapsible

--- src/app/[identifier]/page.tsx
+++ src/app/[identifier]/page.tsx
@@ -1,4 +1,11 @@
 import { fetchCastAndFidData } from "@/lib/utils";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { CollapsibleCodeComponent } from "@/components/collapsible-code";
+
 
 interface ResponseProps {
   params: { identifier: string };
@@ -33,88 +40,102 @@ export default async function Page({ params }: ResponseProps) {
       <div className="w-full flex flex-col md:flex-row justify-center items-start p-2 space-x-0 md:space-x-8 space-y-4 md:space-y-0">
         {warpcastAuthor && (
           <div>
-            <h3>
+         
+            <CollapsibleCodeComponent triggerContent={    <h3 className="text-center">
               Warpcast API FID Response: {warpcastAuthor?.error ? '❌' : '✅'} ({warpcastAuthor?.durationInMs?.toFixed(2)} ms)
-            </h3>
+            </h3>}>
             <pre className="bg-gray-800 text-white p-2 rounded w-full md:w-auto font-mono text-sm max-w-lg max-h-72 overflow-y-scroll overflow-x-scroll">
               {JSON.stringify(warpcastAuthor, null, 2)}
             </pre>
+            </CollapsibleCodeComponent> 
           </div>
         )}
         {warpcastCast && (
           <div>
-            <h3>
+            <CollapsibleCodeComponent triggerContent={   <h3 className="text-center">
               Warpcast API Cast Response: {warpcastCast?.error ? '❌' : '✅'} ({warpcastCast?.durationInMs?.toFixed(2)} ms)
-            </h3>
+            </h3>}>
             <pre className="bg-gray-800 text-white p-2 rounded w-full md:w-auto font-mono text-sm max-w-lg max-h-72 overflow-y-scroll overflow-x-scroll">
               {JSON.stringify(warpcastCast, null, 2)}
             </pre>
+            </CollapsibleCodeComponent>
           </div>
         )}
       </div>
       <div className="w-full flex flex-col md:flex-row justify-center items-start p-2 space-x-0 md:space-x-8 space-y-4 md:space-y-0">
         {nemesAuthor && (
           <div>
-            <h3>
+        
+            <CollapsibleCodeComponent triggerContent={    <h3 className="text-center">
               Warpcast Hub FID Response: {nemesAuthor?.error ? '❌' : '✅'} ({nemesAuthor?.durationInMs?.toFixed(2)} ms)
-            </h3>
+            </h3>}>
             <pre className="bg-gray-800 text-white p-2 rounded w-full md:w-auto font-mono text-sm max-w-lg max-h-72 overflow-y-scroll overflow-x-scroll">
               {JSON.stringify(nemesAuthor, null, 2)}
             </pre>
+            </CollapsibleCodeComponent>
           </div>
         )}
         {nemesCast && (
           <div>
-            <h3>
+          
+            <CollapsibleCodeComponent triggerContent={   <h3 className="text-center">
               Warpcast Hub Cast Response: {nemesCast?.error ? '❌' : '✅'} ({nemesCast?.durationInMs?.toFixed(2)} ms)
-            </h3>
+            </h3>}>
             <pre className="bg-gray-800 text-white p-2 rounded w-full md:w-auto font-mono text-sm max-w-lg max-h-72 overflow-y-scroll overflow-x-scroll">
               {JSON.stringify(nemesCast, null, 2)}
             </pre>
+            </CollapsibleCodeComponent>
           </div>
         )}
       </div>
       <div className="w-full flex flex-col md:flex-row justify-center items-start p-2 space-x-0 md:space-x-8 space-y-4 md:space-y-0">
         {neynarHubAuthor && (
           <div>
-            <h3>
+            <CollapsibleCodeComponent triggerContent={   <h3 className="text-center">
               Neynar Hub FID Response: {neynarHubAuthor?.error ? '❌' : '✅'} ({neynarHubAuthor?.durationInMs?.toFixed(2)} ms)
-            </h3>
+            </h3>}>
             <pre className="bg-gray-800 text-white p-2 rounded w-full md:w-auto font-mono text-sm max-w-lg max-h-72 overflow-y-scroll overflow-x-scroll">
               {JSON.stringify(neynarHubAuthor, null, 2)}
             </pre>
+            </CollapsibleCodeComponent>
           </div>
         )}
         {neynarHubCast && (
           <div>
-            <h3>
+         
+            <CollapsibleCodeComponent triggerContent={   <h3 className="text-center">
               Neynar Hub Cast Response: {neynarHubCast?.error ? '❌' : '✅'} ({neynarHubCast?.durationInMs?.toFixed(2)} ms)
-            </h3>
+            </h3>}>
             <pre className="bg-gray-800 text-white p-2 rounded w-full md:w-auto font-mono text-sm max-w-lg max-h-72 overflow-y-scroll overflow-x-scroll">
               {JSON.stringify(neynarHubCast, null, 2)}
             </pre>
+            </CollapsibleCodeComponent>
           </div>
         )}
       </div>
       <div className="w-full flex flex-col md:flex-row justify-center items-start p-2 space-x-0 md:space-x-8 space-y-4 md:space-y-0">
         {neynarAuthor && (
           <div>
-            <h3>
+        
+            <CollapsibleCodeComponent triggerContent={  <h3 className="text-center">
               Neynar API FID Response: {neynarAuthor?.error ? '❌' : '✅'} ({neynarAuthor?.durationInMs?.toFixed(2)} ms)
-            </h3>
+            </h3>}> 
             <pre className="bg-gray-800 text-white p-2 rounded w-full md:w-auto font-mono text-sm max-w-lg max-h-72 overflow-y-scroll overflow-x-scroll">
               {JSON.stringify(neynarAuthor, null, 2)}
             </pre>
+            </CollapsibleCodeComponent>
           </div>
         )}
         {neynarCast && (
           <div>
-            <h3>
+       
+            <CollapsibleCodeComponent triggerContent={  <h3 className="text-center">
               Neynar API Cast Response: {neynarCast?.error ? '❌' : '✅'} ({neynarCast?.durationInMs?.toFixed(2)} ms)
-            </h3>
+            </h3>}>
             <pre className="bg-gray-800 text-white p-2 rounded w-full md:w-auto font-mono text-sm max-w-lg max-h-72 overflow-y-scroll overflow-x-scroll">
               {JSON.stringify(neynarCast, null, 2)}
             </pre>
+            </CollapsibleCodeComponent>
           </div>
         )}
       </div>
