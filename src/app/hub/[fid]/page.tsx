593ebef8da9a8ea91ba46533aa9d68a2bd825dcd
shamikaga <shamik.aga@gmail.com>
2024-06-03 21:25:53 +0100

    added support for 2 gRPC urls

--- src/app/hub/[fid]/page.tsx
+++ src/app/hub/[fid]/page.tsx
@@ -6,35 +6,48 @@ import ReactDiffViewer from 'react-diff-viewer-continued';
 
 interface Props {
   params: { fid: number };
-  searchParams: { hub: string};
+  searchParams: { hub1: string, hub2?: string};
 }
 
 export default function Page(props: Props) {
   
-  const [resp, setResp] = useState<JSON>();
+  const [resp1, setResp1] = useState<JSON>();
+  const [resp2, setResp2] = useState<JSON>();
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData1 = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/api/get_user_data/${props.params.fid}/${props.searchParams.hub}`)
-        setResp(response.data)
+        const response = await axios.get(`http://localhost:3000/api/get_user_data/${props.params.fid}/${props.searchParams.hub1}`)
+        setResp1(response.data)
       } catch (error) {
-        setResp(error as JSON)
+        setResp1(error as JSON)
       }
     };
 
-    fetchData();
-  }, [props.searchParams.hub, props.params.fid]); // Dependency array
+    const fetchData2 = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3000/api/get_user_data/${props.params.fid}/${props.searchParams.hub2}`)
+        setResp2(response.data)
+      } catch (error) {
+        setResp2(error as JSON)
+      }
+    };
+
+    fetchData1();
+    if (props.searchParams.hub2?.length ?? 0 > 0) {
+      fetchData2()
+    }
+  }, [props.searchParams.hub1, props.searchParams.hub2, props.params.fid]); // Dependency array
   
 
   return (
     <div className="p-2">
-      {resp === undefined ? (
+      {resp1 === undefined ? (
         <div>Loading...</div>
       ) : (
         <div className="flex justify-center w-full">
           <pre className="bg-gray-800 text-white p-2 rounded w-full font-mono text-sm overflow-y-scroll overflow-x-scroll">
-            <ReactDiffViewer oldValue={JSON.stringify(resp, null, 2)} newValue={JSON.stringify(resp, null, 2)} splitView={true} />
+            <ReactDiffViewer oldValue={JSON.stringify(resp1, null, 2)} newValue={JSON.stringify(resp2, null, 2)} splitView={true} />
           </pre>
         </div>
       )}
--- src/app/hub/page.tsx
+++ src/app/hub/page.tsx
@@ -6,7 +6,8 @@ import { useRouter } from 'next/navigation';
 import { useState } from "react";
 
 export default function Page() {
-  const [hubUrl, setHubUrl] = useState<string>("")
+  const [hubUrl1, setHubUrl1] = useState<string>("")
+  const [hubUrl2, setHubUrl2] = useState<string>("")
   const [fID, setFID] = useState<string>("")
   const [error, setError] = useState<string | null>()
   const router = useRouter()
@@ -15,7 +16,7 @@ export default function Page() {
     const fIdNumber = parseInt(fID)
     if (fIdNumber) {
       setError(null)
-      router.push(`/hub/${fID}?hub=${hubUrl}`)
+      router.push(`/hub/${fID}?hub1=${hubUrl1}&hub2=${hubUrl2}`)
     } else {
       setError("fID must be a number")
     }
@@ -26,16 +27,22 @@ export default function Page() {
       <p>Explore User Data in Hubs</p>
       <Input 
         className=""
-        placeholder="Enter Hub gRPC url"
-        value={hubUrl}
-        onChange={(e)=>{ setHubUrl(e.target.value)}}
-       />
-      <Input 
-        className=""
         placeholder="Enter fID"
         value={fID}
         onChange={(e)=>{ setFID(e.target.value) }}
       />
+      <Input 
+        className=""
+        placeholder="Enter Hub gRPC url 1"
+        value={hubUrl1}
+        onChange={(e)=>{ setHubUrl1(e.target.value)}}
+       />
+       <Input 
+        className=""
+        placeholder="Enter Hub gRPC url 2 (optional)"
+        value={hubUrl2}
+        onChange={(e)=>{ setHubUrl2(e.target.value)}}
+       />
       <Button onClick={handleSubmit}>
         Submit
       </Button>
