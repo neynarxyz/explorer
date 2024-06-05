23e15840b8c59650975abdbb931edaf3ae5bffa5
dylsteck <dylancsteck@gmail.com>
2024-06-05 16:08:05 -0700

    fix: mobile responsiveness and children vertical alignment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,18 +27,18 @@ export default function RootLayout({
       </head>
       <body className="relative h-screen flex flex-col items-center min-h-screen">
         <Providers>
-          <div className="absolute top-0 left-0 p-2">
-            <Link href="https://www.neynar.com" target="_blank">
-              <img className="w-16 md:w-32" src="/neynar.png" />
-            </Link>
-          </div>
-          <Card className="min-w-[55%] md:min-w-[40%] fixed top-5 left-1/2 transform -translate-x-1/2 p-4 mb-5 z-10">
-            <Link href="/" className="text-black font-bold">
-              <p className="mb-4 text-lg md:text-">Farcaster Explorer</p>
-            </Link>
-            <Search />
-          </Card>
-          <div className="w-full mt-20 px-4 md:px-8 flex flex-col items-center">
+          <div className="w-1/2 mt-5 px-4 md:px-8 flex flex-col items-center">
+            <div className="m-auto relative">
+              <Link href="https://www.neynar.com" target="_blank">
+                  <img className="w-16 md:w-32" src="/neynar.png" />
+              </Link>
+            </div>
+            <Card className="w-[100%] p-3">
+              <Link href="/" className="text-black font-bold">
+                <p className="mb-4 text-lg md:text-">Farcaster Explorer</p>
+              </Link>
+              <Search />
+            </Card>
             {children}
           </div>
         </Providers>
