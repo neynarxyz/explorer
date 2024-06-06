5c5f275481a786b064f9499a041674d2246b94dc
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-06-05 22:27:21 -0400

    Revert "add: latest casts list and small styling improvements"

--- src/components/profile-component.tsx
+++ src/components/profile-component.tsx
@@ -1,26 +1,20 @@
-//note: not using this in favor of https://github.com/pugson/react-farcaster-embed, but this is a good example of how to render a cast.
-
 import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar"
 import { CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
-import Link from 'next/link'
+//note: not using this in favor of https://github.com/pugson/react-farcaster-embed, but this is a good example of how to render a cast.
+import Link from 'next/link';
 
-export function ProfileComponent({ pfp, url, headerText }: any) {
-  if (!pfp) return null
-
+export function ProfileComponent({pfp,url}: any) {
+  if(!pfp) return null;
   return (
     <Link href={`/${encodeURIComponent(url)}`}>
-      <Card className="w-full hover:bg-slate-100 flex flex-col rounded-lg max-w-md md:min-w-64 md:min-h-32 items-center justify-center">
-      {headerText && headerText.length > 0 &&
-            <div className="pl-3 pb-1.5 border-b border-black/75 w-full">
-              <p className="font-medium text-black/90">{headerText}</p>
-            </div>
-        }
-        <CardContent className="p-4 w-full flex flex-col justify-center items-center space-y-10">
-          <Avatar>
-            <AvatarImage alt={`${url} PFP`} src={pfp} />
-          </Avatar>
-        </CardContent>
-      </Card>
+    <Card className="w-full hover:bg-slate-100 rounded-lg max-w-md md:min-w-64 md:min-h-32 flex items-center justify-center">
+      <CardContent className="p-4 w-full flex flex-col justify-center items-center space-y-10">
+      <Avatar >
+    <AvatarImage alt={`${url} PFP`} src={pfp} />
+  </Avatar>
+      </CardContent>
+    </Card>
     </Link>
   )
 }
+
